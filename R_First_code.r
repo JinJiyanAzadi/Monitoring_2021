48280a79ab0a9f196b0303e33cd73852cbd678b4
JinJiyanAzadi <73694409+JinJiyanAzadi@users.noreply.github.com>
2020-11-02 12:32:13 +0100

    Update R_First_code.r

--- R_First_code.r
+++ R_First_code.r
@@ -1,31 +1,33 @@
-# Write what you wish!
+# Welcome all to the dark side! <3
 
-primates <- c(3, 5, 9, 15, 40) #set of individuals observed, this is an array of data
-primate enter
+primates <- c(3, 5, 9, 15, 40) # set of individuals observed, this is an array of data
+primate (enter)
 
-#new set
+# new set
 crabs <- c(100, 70, 30, 10, 5)
 
 # first plot in R
-plot(primates, crabs)      # this is the function to plot the data
-#change the color of the plot
+plot(primates, crabs) # this is the function to plot the data
+
+# change the color of the plot
 plot(primates, crabs, col="red")
 
-plot(primates, crabs, col="red", pch=19)  # change the point carachter pch - see the list from internet each point has a code numebr
+# change the point character pch - each pch has a code number to be imputed(find it on the manual or internet)
+plot(primates, crabs, col="red", pch=19)  
 
-#add new argument - carachter exageration cex
-plot(primates, crabs, col="red", pch=19, cex=2)
-plot(primates, crabs, col="red", pch=19, cex=6)
+# add a new argument: character exageration "cex"
+plot(primates, crabs, col="red", pch=19, cex=2) # example with cex=2
+plot(primates, crabs, col="red", pch=19, cex=6) # example with cex=6
 
-#add a title
+# add a title to my plot
 plot(primates, crabs, col="red", pch=19, cex=6, main="My first ecological graph in R!")
 
-#create a dataframe - table 
+# create a dataframe i.e. a table containing my data 
 ecoset <- data.frame(primates, crabs)
 
 # let's ask an ecological question: what's the mean of my data set?
-#mean numb of individuals per site
+# for example mean numb of individuals per site?
 (3+5+9+15+40)/5 # average primates per site
 
-#eaier way using the summary function of the dataframe
+# eaier way is using the summary function of the dataframe
 summary(ecoset)
