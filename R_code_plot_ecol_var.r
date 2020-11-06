f58a3a7d28b0eda19f1e41ff96e245c9718d1f1e
JinJiyanAzadi <73694409+JinJiyanAzadi@users.noreply.github.com>
2020-11-06 11:07:46 +0100

    Update R_code_plot_ecol_var.r

--- R_code_plot_ecol_var.r
+++ R_code_plot_ecol_var.r
@@ -10,6 +10,25 @@ library(sp) # we do not use the quotes as we are already in R
 data(meuse)  
 
 View(meuse) # the table will pop out showing all the data
-head(meuse)ta
+head(meuse)
 
-#ex --> find the mean of all the values
+# Exercise: find the mean of all the values --< Solution: from past lecture we used the summary f to avoid to do it manually
+summary(meuse) # it summarises all the UNIVARIANTs statistics of the observed dataset
+
+# Ex 2: plot Zinc (y) against Cadmium (x) 
+plot(cadmium, zinc) 
+# --> Error in plot(cadmium, zinc) : oggetto "cadmium" non trovato // because I did not show R where to find the data
+
+#Solution: use the $ to link the data
+plot(meuse$cadmium, meuse$zinc) 
+
+# To get the same result we can use the "attach f"
+attach(meuse)
+plot(cadmium, zinc) 
+# I will get the same plot as the one made by using the $ option
+
+# How to impress yr supervisor :D
+# scareplot matrixes
+pairs(meuse)
+
+# Ex 3: how to pair only certain elements of the dataset? 
