90277280ac9a8c204fa1059b1c393f61b70c9b72
JinJiyanAzadi <73694409+JinJiyanAzadi@users.noreply.github.com>
2020-11-26 17:22:43 +0100

    Update R_Personal_Exercises.r

--- R_Personal_Exercises.r
+++ R_Personal_Exercises.r
@@ -1,5 +1,14 @@
-# Code suggested by the prof to create a density map with the points' size resembling the nu,ber of cases. I changed the previous palette colours:
+# Code suggested by the prof to create a density map with the points' size resembling the number of cases. I changed the previous palette colours
 
+# original code by the prof
+library(sf)
+Spoints <- st_as_sf(covid, coords = c("lon", "lat"))
+
+plot(density_map)
+# plot(Spoints, cex=Spoints$cases, add=T)
+plot(Spoints, cex=Spoints$cases/10000, add=T)
+
+# My changes
 > cl <- colorRampPalette(c('lightpink2','lightsalmon','tomato1','red3','maroon'))(100)
 > plot(density_map, col = cl)
 > plot(coastlines, add = T)
