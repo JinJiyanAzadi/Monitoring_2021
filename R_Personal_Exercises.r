9c89885cd7a2d989cc3817205f15aa92df6bcb17
JinJiyanAzadi <73694409+JinJiyanAzadi@users.noreply.github.com>
2020-11-26 18:27:31 +0100

    Update R_Personal_Exercises.r

--- R_Personal_Exercises.r
+++ R_Personal_Exercises.r
@@ -9,10 +9,13 @@ plot(density_map)
 plot(Spoints, cex=Spoints$cases/10000, add=T)
 
 # My changes
-> cl <- colorRampPalette(c('lightpink2','lightsalmon','tomato1','red3','maroon'))(100)
-> plot(density_map, col = cl)
-> plot(coastlines, add = T)
-> plot(Spoints, cex=Spoints$cases/10000, add=T)
+cl <- colorRampPalette(c('lightpink2','lightsalmon','tomato1','red3','maroon'))(100)
+plot(density_map, col = cl)
+plot(coastlines, add = T)
+plot(Spoints, cex=Spoints$cases/10000, add=T)
 # Warning message:
 # In plot.sf(Spoints, cex = Spoints$cases/10000, add = T) :
 # ignoring all but the first attribute
+
+# In order to increase the thikness of the circles' line use the "lwd" arg and to unify the colour, use the "col" arg:
+plot(Spoints, cex=Spoints$cases/10000, col = 'purple3', lwd = 3, add=T)
