578c07f62b2cc336f1d5a65661e0ae33e25c41a0
JinJiyanAzadi <73694409+JinJiyanAzadi@users.noreply.github.com>
2020-11-29 16:59:08 +0100

    Create R_code_Leo_data.r

---
+++ R_code_Leo_data.r
@@ -0,0 +1,27 @@
+# 29.11.2020
+# Exercise based upon data given by our classmate Leo on Venice's lagoon
+
+leo <- read.table("dati_zabotti.csv", head=T, sep=",")
+
+attach(leo)
+
+leo_ppp <- ppp(x, y, c(2300000,2325000), c(5005000,5045000))
+
+marks(leo_ppp) <- chlh
+chlh_map <- Smooth(leo_ppp)
+plot(chlh_map)
+
+marks(leo_ppp) <- t
+t_map <- Smooth(leo_ppp)
+plot(t_map)
+points(leo_ppp)
+
+par(mfrow=c(1,2))
+plot(chlh_map)
+points(leo_ppp)
+
+plot(t_map)
+points(leo_ppp)
+
+
+# Exercise: do the same for chlorophyll in the sediment (chls)
