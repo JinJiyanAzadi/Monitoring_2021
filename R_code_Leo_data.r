84de4a7f05e1778ec16a3132a3d92186c5628976
JinJiyanAzadi <73694409+JinJiyanAzadi@users.noreply.github.com>
2020-11-29 18:07:37 +0100

    Update R_code_Leo_data.r

--- R_code_Leo_data.r
+++ R_code_Leo_data.r
@@ -29,7 +29,25 @@ points(leo_ppp)
 library(spatstat)
 
 setwd("D:/Utenti/Norma/Desktop/lab")
-leo <- read.table("dati_zabotti.csv, head = T, sep = ",")
-attach(leo)
-leo_pp <- ppp(x, y, c(2300000, 2325000), c(5005000, 5045000))
 
+leo <- read.table("dati_zabotti.csv, head = T, sep = ",")
+
+attach(leo)
+
+leo_ppp <- ppp(x, y, c(2300000, 2325000), c(5005000, 5045000))
+
+marks(leo_ppp) <- chls
+chls_map <- Smooth(leo_ppp)
+plot(chls_map)
+
+marks(leo_ppp) <- t
+t_map <- Smooth(leo_ppp)
+plot(t_map)
+points(leo_ppp)
+
+par(mfrow=c(1,2))
+plot(chls_map)
+points(leo_ppp)
+
+plot(t_map)
+points(leo_ppp)
