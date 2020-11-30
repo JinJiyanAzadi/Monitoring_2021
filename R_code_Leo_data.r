56fde88b18cf160de08000d3f9c406f658d86f9d
JinJiyanAzadi <73694409+JinJiyanAzadi@users.noreply.github.com>
2020-11-30 10:56:08 +0100

    Update R_code_Leo_data.r

--- R_code_Leo_data.r
+++ R_code_Leo_data.r
@@ -66,3 +66,20 @@ points(leo_ppp, col = "red", lwd = 2)
 
 plot(t_map)
 points(leo_ppp, col = "red", lwd = 2)
+
+# Changes during the class 30.11.2020
+
+library(spatstat)
+
+setwd("D:/Utenti/Norma/Desktop/lab")
+
+leo <- read.table("dati_zabotti.csv, head = T, sep = ",")
+
+attach(leo)
+
+leo_ppp <- ppp(x, y, c(2300000, 2325000), c(5005000, 5045000))
+
+plot(leo_ppp)
+density_map <- density(leo_ppp)
+plot(density_map)
+points(leo_ppp)
