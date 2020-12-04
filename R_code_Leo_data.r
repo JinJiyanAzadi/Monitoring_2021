206a00becec3112ceaea47237d86ca6f5c7ec2c7
JinJiyanAzadi <73694409+JinJiyanAzadi@users.noreply.github.com>
2020-12-04 09:59:25 +0100

    Update R_code_Leo_data.r

--- R_code_Leo_data.r
+++ R_code_Leo_data.r
@@ -75,3 +75,42 @@ plot(leo_ppp)
 density_map <- density(leo_ppp)
 plot(density_map)
 points(leo_ppp)
+
+
+# 04.12.2020 -- in classe
+# Interpolation (linear)
+# First we upload the saved .RData worksheet
+
+setwd("D:/Utenti/Norma/Desktop/lab")
+load("point_pattern_analysis.RData")
+list()
+
+# next, explain to R the variable we are going to use
+library(spatstat) # as the marks f is in this package
+attach(leo)
+leo_ppp <- ppp(x, y, c(2300000, 2325000), c(5005000, 5045000))
+marks(leo_ppp) <- chls
+
+# first, we change the collour palette to avoid too much blue
+cl <- colorRampPalette(c('yellow','orange','red','green'))(100) 
+plot(chlh_map, col=cl)
+chls_map <- Smooth(leo_ppp)
+points(leo_ppp)
+
+setwd("D:/Utenti/Norma/Desktop/lab")
+leo_ppp <- ppp(x, y, c(2300000, 2325000), c(5005000, 5045000))
+attach(leo)
+plot(leo_ppp)
+density_map <- density(leo_ppp)
+plot(density_map)
+cl <- colorRampPalette(c('yellow','orange','red','green'))(100) # to change the palatte
+plot(density_map, col = cl)
+points(leo_ppp)
+
+# map the chls now (see my code above) 
+
+marks(leo_ppp) <- chls
+chls_map <- Smooth(leo_ppp)
+plot(chls_map, col = cl)
+points(leo_ppp)
+
