e1781237323e38ac5bfba35bb37673cb360f881d
JinJiyanAzadi <73694409+JinJiyanAzadi@users.noreply.github.com>
2021-01-07 18:04:00 +0100

    Update R_code_Remote_sensing.r

--- R_code_Remote_sensing.r
+++ R_code_Remote_sensing.r
@@ -1,3 +1,4 @@
 # 21.12.2020
 # R code for remote sensing analysis to monitor ecosystem changes in space and time
-install.packages(c("raster", "RStoolbox"))
+install.packages(c("raster", "RStoolbox")) # raster data are based on images i.e. pixels + RStoolbox is used for remote sensing data analysis 
+
