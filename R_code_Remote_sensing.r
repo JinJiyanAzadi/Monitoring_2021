7da63369480268e1700b66bcb765d903913dfb7d
JinJiyanAzadi <73694409+JinJiyanAzadi@users.noreply.github.com>
2021-01-08 09:48:10 +0100

    Update R_code_Remote_sensing.r

--- R_code_Remote_sensing.r
+++ R_code_Remote_sensing.r
@@ -1,4 +1,6 @@
-# 21.12.2020
+# 08.01.2021
 # R code for remote sensing analysis to monitor ecosystem changes in space and time
 install.packages(c("raster", "RStoolbox")) # raster data are based on images i.e. pixels + RStoolbox is used for remote sensing data analysis 
 
+# Now as usual we set the wd
+setwd(
