a3930dc31c92d7c5ff5fe832817764e3b7053714
JinJiyanAzadi <73694409+JinJiyanAzadi@users.noreply.github.com>
2021-01-11 10:06:16 +0100

    Update R_code_Exam_example.r

--- R_code_Exam_example.r
+++ R_code_Exam_example.r
@@ -5,7 +5,8 @@ install.packages("ncdf4")
 library(ncdf4) # the .nc format is the one related to the ESA data. This library will read these type of files
 library(raster)
 
-setwd("D:")
+setwd("D:/Utenti/Norma/Desktop/lab")
+
 
 t <- raster("c_gls_LST10-DC_202012210000_GLOBE_GEO_V1.2.1.nc")
 
