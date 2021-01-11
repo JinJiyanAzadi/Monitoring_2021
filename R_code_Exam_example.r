1c503355ee7c508ad76cbdffb87708553ade9312
JinJiyanAzadi <73694409+JinJiyanAzadi@users.noreply.github.com>
2021-01-11 11:15:44 +0100

    Update R_code_Exam_example.r

--- R_code_Exam_example.r
+++ R_code_Exam_example.r
@@ -4,7 +4,7 @@
 
 install.packages("ncdf4")
 
-library(ncdf4) # the .nc format is the one related to the ESA data. This library will read these type of files
+library(ncdf4) # the .nc format is the one related to the ESA data. This library will read these types of files
 library(raster)
 setwd("D:/Utenti/Norma/Desktop/lab")
 
@@ -27,14 +27,16 @@ plot(tjan, col=cltjan)
 toct20 <- raster("c_gls_LST10-DC_202010010000_GLOBE_GEO_V1.2.1.nc") 
 plot(toct20)
 
+# Now we plot the differences in T° during the two periods examined
 
 dift <- tjan21 - toct20
 cldif <- colorRampPalette(c('blue','white','red'))(100) #
-plot(dift, col=cldif)
+plot(dift, col=cldif, main="T° Jan21 vs Oct20")
 
 
-# difference in time between the two images
-dif <- tjan21-toct20
-plot(dif)
+# To plot them together
+par(mfrow=c(2,1))
+plot(tjan21, cl=cltjan21, main="T° Jan21")
+plot(toct20, cl=cltjan21, main="T° Oct20")
 
-par(mfrow, c=
+
