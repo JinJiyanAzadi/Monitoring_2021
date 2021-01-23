4904891dec99588ddac7ba7b641b0f484723aa4d
JinJiyanAzadi <73694409+JinJiyanAzadi@users.noreply.github.com>
2021-01-23 23:10:35 +0100

    Update R_Code_Himalayans_Ice_Cover_Exam.r

--- R_Code_Himalayans_Ice_Cover_Exam.r
+++ R_Code_Himalayans_Ice_Cover_Exam.r
@@ -3,17 +3,25 @@
 library(raster) # raster data i.e. images(pixels)
 library(RStoolbox) #  for remote sensing
 library(rgdal) # coastlines
-library(ncdf4) # when using .nc files
+library(ncdf4) # when using .nc files // the .nc format is the one related to the ESA data. This library will read these types of files
 
 setwd("D:/Utenti/Norma/Desktop/lab")
 
-p224r63_2011 <- brick("c_gls_SCE_202101210000_NHEMI_VIIRS_V1.0.1.nc")
+IC_Jan21 <- brick("c_gls_SCE_202101210000_NHEMI_VIIRS_V1.0.1.nc")
+IC_Jan21 # to see the carachteristics of the data
+plot(IC_Jan21) # we are not happy with the colours so we will change it but forst we add the coastlines
 
-
+# Adding the coastlines
 coastlines <- readOGR('ne_10m_coastline.shp')
 # coastlines_simp <- gSimplify(coastlines, tol = 3, topologyPreserve = TRUE)
 
-plot(coastlines, add = T)
+# We crop the coastlines to match our image
+# To do so we take the extent of the image then we crop it with the exact extent using the "ext" fixed argument
+ext <- c(-180, 180, 25, 84)  # xmin xmax ymin ymax // ext is a fixed argument, we cannot change the name
+
+# Then we create a second argument which is the cropped following the coordinates in the argument "ext"
+Coast_Crop <- crop(coastlines, ext) 
+plot(coastlines, add = T) # the argument "add=T" is needed to add the coastlines on top of my current image
 
 cl <- colorRampPalette(c('lightblue','blues','white'))(100) # 
 cl <- choose_palett(name from the list)
@@ -25,9 +33,6 @@ tjan21 <- raster("c_gls_SCE_202101210000_NHEMI_VIIRS_V1.0.1.nc")
 
 copNDVI <- reclassify(copNDVI, cbind(253:255, NA)) 
 
-ext <- c(0,20,35,55)  # xmin xmax ymin ymax // ext is a fixed argument, cannot change the name
-copNDVI_Italy <- crop(copNDVI, ext)
-plot(copNDVI_Italy, col=clymax) # I did this with the coastlines too
 
 # https://www.r-bloggers.com/2018/10/the-av-package-production-quality-video-in-r/
 # Library to plot the video, check BYS
