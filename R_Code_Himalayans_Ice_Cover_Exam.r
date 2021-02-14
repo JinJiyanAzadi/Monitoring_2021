ad4b0574fda58911daef781e115bb7d6a9b9d91b
JinJiyanAzadi <73694409+JinJiyanAzadi@users.noreply.github.com>
2021-02-14 13:06:49 +0100

    Update R_Code_Himalayans_Ice_Cover_Exam.r

--- R_Code_Himalayans_Ice_Cover_Exam.r
+++ R_Code_Himalayans_Ice_Cover_Exam.r
@@ -75,14 +75,12 @@ dev.off()
 boxplot(ALDH_15yr_c, horizontal=F, axis=T, outline=F, col="royalblue", main="Mean Annual_Feb Albedo variation", xlab=" Period 2000-2015")
 # Need to fix the labeling
 
-#Try the line variation among the most diverse year 
+
+# Set the colours palette for the plotting   
+cl <- colorRampPalette(c('white','lightblue','midnightblue', 'tan2', 'yellow','orangered4'))(100) 
 
 
-# We set the colours palette for the plotting   
-cl <- colorRampPalette(c('white','lightblue','midnightblue', 'tan2', 'yellow','orangered4'))(100) # preferito --> MIGLIORE
-
-
-# We plot the images in two rounds to allow showing the label on each image 
+# Plot the images in two rounds to allow showing the label on each image 
 par(mfrow=c(3,4)) 
 plot(AL_Feb00_c, main="Feb_00", col=cl)
 plot(AL_Feb01_c, main="Feb_01", col=cl)
@@ -106,20 +104,29 @@ plot(AL_Feb15_c, main="Feb_15", col=cl)
 
 ##############################################
 
+# Prepare the ground for the time series and forecast based on the albedo details, we extrapolate the min/max albedo values from the stack ALDH_15yr_c
+
 > ALDH_15yr_c
-class      : RasterStack 
-dimensions : 1344, 3136, 4214784, 16  (nrow, ncol, ncell, nlayers)
-resolution : 0.008928571, 0.008928571  (x, y)
-extent     : 67.99554, 95.99554, 26.00446, 38.00446  (xmin, xmax, ymin, ymax)
-crs        : +proj=longlat +ellps=WGS84 +no_defs 
-names      : Broadband//spectrum.1, Broadband//spectrum.2, Broadband//spectrum.3, Broadband//spectrum.4, Broadband//spectrum.5, Broadband//spectrum.6, Broadband//spectrum.7, Broadband//spectrum.8, Broadband//spectrum.9, Broadband//pectrum.10, Broadband//pectrum.11, Broadband//pectrum.12, Broadband//pectrum.13, Broadband//pectrum.14, Broadband//pectrum.15, ... 
+#class      : RasterStack 
+#dimensions : 1344, 3136, 4214784, 16  (nrow, ncol, ncell, nlayers)
+#resolution : 0.008928571, 0.008928571  (x, y)
+#extent     : 67.99554, 95.99554, 26.00446, 38.00446  (xmin, xmax, ymin, ymax)
+#crs        : +proj=longlat +ellps=WGS84 +no_defs 
+#names      : Broadband//spectrum.1, Broadband//spectrum.2, Broadband//spectrum.3, Broadband//spectrum.4, Broadband//spectrum.5, Broadband//spectrum.6, Broadband//spectrum.7, Broadband//spectrum.8, Broadband//spectrum.9, Broadband//pectrum.10, Broadband//pectrum.11, Broadband//pectrum.12, Broadband//pectrum.13, Broadband//pectrum.14, Broadband//pectrum.15, ... 
 min values :                0.0199,                0.0213,                0.0220,                0.0218,                0.0228,                0.0208,                0.0217,                0.0227,                0.0221,                0.0210,                0.0199,                0.0203,                0.0240,                0.0192,                0.0200, ... 
 max values :                0.7790,                0.7733,                0.7796,                0.7038,                0.7673,                0.7616,                0.7731,                0.7619,                0.7771,                0.7760,                0.7735,                0.7830,                0.7782,                0.7794,                0.7786, ... 
 
-> min_values <- c(0.0199, 0.0213, 0.0220, 0.0218, 0.0228, 0.0208, 0.0217, 0.0227, 0.0221, 0.0210, 0.0199,0.0203,0.0240, 0.0192,0.0200)
-> max_values<- c(0.7790, 0.7733, 0.7796, 0.7038, 0.7673,  0.7616, 0.7731, 0.7619, 0.7771, 0.7760, 0.7735, 0.7830, 0.7782, 0.7794, 0.7786)
-> df <- data.frame(min_values, max_values)
-> df
+# Assign the min/max albedo values to two different variables called "min_values" & "max_values" respectivelly 
+min_values <- c(0.0199, 0.0213, 0.0220, 0.0218, 0.0228, 0.0208, 0.0217, 0.0227, 0.0221, 0.0210, 0.0199,0.0203,0.0240, 0.0192,0.0200)
+max_values<- c(0.7790, 0.7733, 0.7796, 0.7038, 0.7673,  0.7616, 0.7731, 0.7619, 0.7771, 0.7760, 0.7735, 0.7830, 0.7782, 0.7794, 0.7786)
+
+# Combine the values together before creating the dat frame. 
+# Note: If the values are not combined before, the time series will result into two different graphs (prior the decomposition), one for the min and one for the max.
+data <- (c(min_values, max_values))
+
+# Create a data frame with these values 
+df <- data.frame(min_values, max_values)
+df
    min_values max_values
 1      0.0199     0.7790
 2      0.0213     0.7733
@@ -138,46 +145,6 @@ max values :                0.7790,                0.7733,                0.7796
 15     0.0200     0.7786
 > 
 #################################################
-    
-
------------------
-cldif <- colorRampPalette(c('orangered4', 'blue', 'yellow', 'red', 'brown'))(100)
-dif <- AL_Feb00_c - AL_Feb15
-
-
-pal <- choose_palette(#Advanced-Sequential-Multi-hue-la quarte da dx + reverse colours)
-pal(50)
-[1] "#1D0B14" "#320B20" "#3E0D26" "#48102B" "#51142F" "#5A1733" "#621B36" "#6A1F39" "#72233C" "#79283D" "#812C3F" "#883040" "#8F3540" "#963A41"
-[15] "#9C3E40" "#A3433F" "#A9483E" "#AF4D3C" "#B55239" "#BB5735" "#C05C30" "#C6622A" "#CB6721" "#D06D13" "#D47200" "#D77800" "#D97F00" "#DB860F"
-[29] "#DC8C1B" "#DE9226" "#DF992F" "#E19F38" "#E2A540" "#E3AB48" "#E5B151" "#E6B759" "#E7BC61" "#E8C269" "#EAC871" "#EBCD79" "#ECD381" "#EDD889"
-[43] "#EFDD91" "#F0E399" "#F2E8A1" "#F4EDA9" "#F5F2B1" "#F7F7B9" "#FAFCC1" "#FCFFC9"
-
-plot(ALDH_15yr_c, col=pal(50))
-
-
-cl <- colorRampPalette(c('orangered4', 'salmon3', 'yellow', 'lightblue', 'royalblue1'))(100) # preferito
-
-
-
-
-
-
-
-# https://www.r-bloggers.com/2018/10/the-av-package-production-quality-video-in-r/
-# Library to plot the video, check BYS
-
-# Exercise two: Mosaic of glaciers and their differences over time
-
-# This data set contains thickness change mosaics that include approximately 650 Himalayan glaciers between 1975 and 2000, and 1040 Himalayan glaciers between 2000 and 2016. 
-# The data were derived from HEXAGON KH-9 and ASTER digital elevation models (DEMs), by fitting robust linear trends to time series of elevation pixels 
-# over the glacier surfaces.
-# National SNow & Ice data center - NASA
-# https://nsidc.org/data/HMA_Glacier_dH_Mosaics/versions/1
-
-Mosaic_Elv_00_16 <- raster("HMA_Glacier_dH_Mosaics_elevationTrend_2000-2016.tif")
-Mosaic_Elv_75_00 <- raster("HMA_Glacier_dH_Mosaics_elevationTrend_1975-2000.tif")
-
-############
 
 sessionInfo() # to get the session info to be given to the public
 Sys.time() # to publish the time at which the script is done
@@ -187,21 +154,6 @@ Sys.time() # to publish the time at which the script is done
 # Include link to data page to see full description
 ###############################################################################################################
 
-# We crop the coastlines to match our image
-# To do so we take the extent of the image then we crop it with the exact extent using the "ext" fixed argument
-ext <- c(-180, 180, 25, 84)  # xmin xmax ymin ymax, where x=long & y=lat // ext is a fixed argument, we cannot change the name
 
-# Then we create a second argument which is the cropped following the coordinates in the argument "ext"
-Coast_Crop <- crop(coastlines, ext) 
-plot(Coast_Crop, add = T) # the argument "add=T" is needed to add the coastlines on top of my current image
-
-cl <- colorRampPalette(c('white', 'gray', 'darkgrey','lightblue','blue','purple'))(100) # 
-cl <- choose_palette(name from the list)
-
-plotRGB(Ic_Jan21_R, r=4, g=3, b=2, stretch="Lin")
-
-IC_Jan21_R <- brick("c_gls_SCE_202101210000_NHEMI_VIIRS_V1.0.1.nc") # when I need to add up the wave bands
-
-IC_Jan21_R <- reclassify(IC_Jan21, cbind(253:255, NA)) 
 
 
